5f79275cb3d04c4cae6759472e405dc348ccc22d
ashwani-kumar-sharma <ashbtech@gmail.com>
2023-06-15 19:37:16 +0530

    ashwani check

--- ashwani/check.js
+++ ashwani/check.js
@@ -1,1 +1,9 @@
 console.log("Hi First Push")
+
+const main = () => {
+    const bigString = "Hello there I am big string."
+    const result = bigString.substring(6,11)
+    console.log(result)
+}
+
+main()
